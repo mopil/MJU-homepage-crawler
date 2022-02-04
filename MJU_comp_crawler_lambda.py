0f6ce513eaaecfe7b6b334ba3114643bf508d6ec
mopil_BSH <mopil1102@gmail.com>
2022-02-04 15:03:40 +0900

    Update MJU_comp_crawler_lambda.py

--- MJU_comp_crawler_lambda.py
+++ MJU_comp_crawler_lambda.py
@@ -4,15 +4,9 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 from selenium.webdriver.common.by import By
 
-api_key = ''
-chat_id = 0
 
-prefix_url = 'http://jw4.mju.ac.kr/user/'
-
-my_bot = telegram.Bot(api_key)
-
-
-def chrome_driver_setting():
+def lambda_handler(event, context):
+    # driver settings
     driver = st.start_drive()
     driver.get('http://jw4.mju.ac.kr/user/cs/index.action')
     driver.switch_to.frame('index_frame')
@@ -21,48 +15,26 @@ def chrome_driver_setting():
     html = driver.page_source
     soup = BeautifulSoup(html, 'html.parser')
     driver.close()
-    return soup
 
-
-def get_all_articles():
-    soup = chrome_driver_setting()
     result = []
-    for i in soup.select("td > a"):
-        title, link = i.text.strip(), prefix_url + i['href']
-        if title == '':
-            continue
-        else:
-            result.append((title, link))
-    return result
-
-
-def get_new_articles():
-    soup = chrome_driver_setting()
-    result = []
-    # 새로운 글만 가져온다 로직 : new 아이콘이 있는 글만 가져오기
+    
+    # new articles crawling
+    prefix_url = 'http://jw4.mju.ac.kr/user/'
     for tag in soup.select("td > a"):
         for sub_tag in tag.descendants:
             if "img" in str(sub_tag):
                 title, link = tag.text.strip(), prefix_url + tag['href']
                 result.append((title, link))
-    return result
-
-
-def auto_crawling(storage):
-    temp = get_new_articles()
+    
+    # telegram message sending
+    api_key = '5251720307:AAEioDUzN6YsqRYE034soQq8N3-DJ8BSX28'
+    chat_id = '@mju_computer_crawling' #1413972042 #my_bot
+    my_bot = telegram.Bot(api_key)
+    
     message = f'명지대학교 컴퓨터공학과 홈페이지\n{datetime.today().strftime("%Y/%m/%d")} 새 글'
-    count = 0
-    for a in temp:
-        if a not in storage:
-            storage.append(a)
+    if not result:
+        message = f'{datetime.today().strftime("%Y/%m/%d")}\n새 글이 없습니다.'
+    else:
+        for a in result:
             message += f'\n{a[0]}\n{a[1]}'
-            count += 1
-    if count == 0:
-        message = '새 글이 없습니다'
     my_bot.sendMessage(chat_id=chat_id, text=message)
-
-
-def lambda_handler(event, context):
-    storage = get_all_articles()
-
-    auto_crawling(storage)
