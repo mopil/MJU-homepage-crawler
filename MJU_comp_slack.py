21178be32cc5c902985e29e5447246b6e5b12d41
mopil1102 <mopil1102@gmail.com>
2022-02-08 18:32:35 +0900

    성능 개선

--- MJU_comp_slack.py
+++ MJU_comp_slack.py
@@ -5,36 +5,65 @@ from datetime import datetime
 from selenium.webdriver.common.by import By
 
 
-def lambda_handler(event, context):
-    # driver settings
+def set_mju_comp_driver(url):
     driver = st.start_drive()
-    driver.get('http://jw4.mju.ac.kr/user/cs/index.action')
+    driver.get(url)
+
+    # additional action
     driver.switch_to.frame('index_frame')
     driver.find_element(By.XPATH,
                         '//*[@id="pageRightT"]/div[2]/div[1]/div/ul/li[1]/ul/div/div/table/tbody/tr/td/table[1]/tbody/tr/td/a/img').click()
+
     html = driver.page_source
     soup = BeautifulSoup(html, 'html.parser')
     driver.close()
+    return soup
 
+
+def crawling_comp(soup):
     result = []
-
     # new articles crawling
     prefix_url = 'http://jw4.mju.ac.kr/user/'
     for tag in soup.select("td > a"):
         for sub_tag in tag.descendants:
             if "img" in str(sub_tag):
                 title, link = tag.text.strip(), prefix_url + tag['href']
-                result.append((title, link))
+                result.append([title, link])
+
+    pattern = datetime.today().strftime("%Y-%m")
+    i = 0
+    for tag in soup.select("td"):
+        if pattern in str(tag):
+            date = tag.text.strip()
+            result[i].append(date)
+            i += 1
+    return result
 
 
-    message = f'명지대학교 컴퓨터공학과 홈페이지\n{datetime.today().strftime("%Y/%m/%d")} 새 글'
-    if not result:
-        message += '\n새 글이 없습니다.'
+def make_message(result, homepage_title, type="default"):
+    if type == "comp":
+        today = datetime.today().strftime("%Y-%m-%d")
     else:
-        for a in result:
-            message += f'\n{a[0]}\n{a[1]}'
+        today = datetime.today().strftime("%Y.%m.%d")
+    message = f'\n{homepage_title}'
+
+    message_exist = False
+    for notice in result:
+        if notice[2] == today:
+            message += f'\n{notice[0]}\n{notice[1]}\n'
+            message_exist = True
+    if not message_exist:
+        message += "\n새 글이 없습니다.\n"
+    return message
+
+
+def lambda_handler(event, context):
+    message = f'{datetime.today().strftime("%Y-%m-%d")} 명지대학교'
+    soup = set_mju_comp_driver('http://jw4.mju.ac.kr/user/cs/index.action')
+    result_comp = crawling_comp(soup)
+    message += make_message(result_comp, "컴퓨터공학과", "comp")
     token = ''
-    channal = '#컴퓨터공학과'
+    channal = '#test'
 
     response = requests.post("https://slack.com/api/chat.postMessage",
                              headers={"Authorization": "Bearer " + token},
--- MJU_school_guide_slack.py
+++ MJU_school_guide_slack.py
@@ -1,36 +1,72 @@
-
-
 import requests
 from bs4 import BeautifulSoup
-import datetime
-
-prefix_url = 'http://www.mju.ac.kr'
-response = requests.get('http://www.mju.ac.kr/mjukr/257/subview.do')
-soup = BeautifulSoup(response.content, 'html.parser')
+from datetime import datetime
 
 
-titles, links = list(), list()
+def make_message(result, homepage_title, type="default"):
+    if type == "comp":
+        today = datetime.today().strftime("%Y-%m-%d")
+    else:
+        today = datetime.today().strftime("%Y.%m.%d")
+    message = f'\n{homepage_title}'
 
-result = []
-for tag in soup.select('.artclLinkView'):
-    title, link = tag.text, prefix_url + tag['href']
-    if "새글" in title:
-        result.append((title, link))
+    message_exist = False
+    for notice in result:
+        if notice[2] == today:
+            message += f'\n{notice[0]}\n{notice[1]}\n'
+            message_exist = True
+    if not message_exist:
+        message += "\n새 글이 없습니다.\n"
 
-message = f'{datetime.datetime.today().strftime("%Y.%m.%d")} 명지대 학사 공지'
-if not result:
-    message += '\n새 글이 없습니다.'
+    return message
 
-for new in result:
-    title = new[0].replace("새글", "").strip()
-    message += f'\n{title}\n{new[1]}\n'
 
+def crawling_school_notice(url):
+    prefix_url = 'http://www.mju.ac.kr'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    result = []
+    for tag in soup.select('.artclLinkView'):
+        title, link = tag.text, prefix_url + tag['href']
+        if "새글" in title:
+            result.append([title, link])
+    pattern = datetime.today().strftime("%Y.%m")
+    i = 0
+    for tag in soup.select('._artclTdRdate'):
+        if pattern in str(tag):
+            result[i].append(tag.text)
+            i += 1
+
+    for new in result:
+        new[0] = new[0].replace("새글", "").strip()
+
+    return result
+
+
+# main logic
+time = datetime.today().strftime("%Y-%m-%d")
+url_list = {
+    "◎학사 공지◎":'http://www.mju.ac.kr/mjukr/257/subview.do',
+    "◎일반 공지◎":'http://www.mju.ac.kr/mjukr/255/subview.do',
+    "◎장학 공지◎":'http://www.mju.ac.kr/mjukr/259/subview.do',
+}
+
+result_list = []
+for url in url_list.values():
+    result_list.append(crawling_school_notice(url))
+total_message = f'{datetime.today().strftime("%Y-%m-%d")} 명지대학교 공지사항 알림'
+i = 0
+for title in url_list.keys():
+    total_message += make_message(result_list[i], title)
+    i += 1
 
 token = ''
-channal = '#학사공지'
+channal = '#test'
 
 response = requests.post("https://slack.com/api/chat.postMessage",
                          headers={"Authorization": "Bearer " + token},
-                         data={"channel": channal, "text": message}
+                         data={"channel": channal, "text": total_message}
                          )
-print(response)
+print(f'{time} lambda executed.')
+print(f'result_list = {result_list}')
+print(f'total_message = {total_message}')
