77ce89ba6b038db314141639215c22472aff2aeb
mopil1102 <mopil1102@gmail.com>
2022-02-12 04:40:03 +0900

    불필요한 코드 삭제

--- module/crawler.py
+++ module/crawler.py
@@ -1,60 +1,32 @@
 from bs4 import BeautifulSoup
-from datetime import datetime
 import requests
 from selenium.webdriver.common.by import By
 from selenium import webdriver
-
-
-def set_driver(url):
-    options = webdriver.ChromeOptions()
-    options.add_argument('headless')
-    driver = webdriver.Chrome('chromedriver.exe', options=options)
-    driver.get(url)
-    html = driver.page_source
-    soup = BeautifulSoup(html, 'html.parser')
-    driver.close()
-    return soup
-
-
-def set_mju_comp_driver(url):
-    options = webdriver.ChromeOptions()
-    options.add_argument('headless')
-    driver = webdriver.Chrome('chromedriver.exe', options=options)
-    driver.get(url)
-
-    # additional action
-    driver.switch_to.frame('index_frame')
-    driver.find_element(By.XPATH,
-                        '//*[@id="pageRightT"]/div[2]/div[1]/div/ul/li[1]/ul/div/div/table/tbody/tr/td/table[1]/tbody/tr/td/a/img').click()
-
-    html = driver.page_source
-    soup = BeautifulSoup(html, 'html.parser')
-    driver.close()
-    return soup
+import message_manager
 
 
 def crawling_mju_computer_notice(url):
-    soup = set_mju_comp_driver(url)
+    # selenium driver setting
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    driver = webdriver.Chrome('chromedriver.exe', options=options)
+    driver.get(url)
+    driver.switch_to.frame('index_frame')
+    driver.find_element(By.XPATH,
+                        '//*[@id="pageRightT"]/div[2]/div[1]/div/ul/li[1]/ul/div/div/table/tbody/tr/td/table[1]/tbody/tr/td/a/img').click()
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+
     result = []
-    # new articles crawling
     prefix_url = 'http://jw4.mju.ac.kr/user/'
     for tag in soup.select("td > a"):
         for sub_tag in tag.descendants:
             if "img" in str(sub_tag):
                 title, link = tag.text.strip(), prefix_url + tag['href']
                 result.append([title, link])
-
-    pattern = datetime.today().strftime("%Y-%m")
-    i = 0
-    for tag in soup.select("td"):
-        if pattern in str(tag):
-            date = tag.text.strip()
-            result[i].append(date)
-            i += 1
     return result
 
 
-def crawling_mju_notice(url):
+def crawling_mju_common_notice(url):
     prefix_url = 'http://www.mju.ac.kr'
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
@@ -62,42 +34,26 @@ def crawling_mju_notice(url):
     for tag in soup.select('.artclLinkView'):
         title, link = tag.text, prefix_url + tag['href']
         if "새글" in title:
-            result.append([title, link])
-    pattern = datetime.today().strftime("%Y.%m")
-
-    i = 0
-    for tag in soup.select('._artclTdRdate'):
-        if i >= len(result): break
-        if pattern in str(tag):
-            result[i].append(tag.text)
-            i += 1
-
-    for new in result:
-        new[0] = new[0].replace("새글", "").strip()
-
+            result.append([title.replace("새글", "").strip(), link])
     return result
 
 
-def crawling_yu_notice(url):
+def crawling_yu_common_notice(url):
     prefix_url = url
     response = requests.get(url, verify=False)
     soup = BeautifulSoup(response.content, 'html.parser')
-    titles, links, dates = list(), list(), list()
-
-    for item in soup.select('.b-title-box > a'):
-        title, link = item['title'], prefix_url + item['href']
-        if title not in titles:
-            titles.append(title)
-        if 'comment' not in link:
-            links.append(link)
-
-    for item in soup.select('.b-date'):
-        dates.append(item.text.strip())
-
     result = []
-    for i in range(len(titles)):
-        result.append([titles[i], links[i], dates[i]])
+    for tag in soup.select('.b-title-box > a'):
+        title, link = tag['title'], prefix_url + tag['href']
+        if 'comment' not in link:
+            result.append([title, link])
     return result
 
 
-
+def main_logic(homepage_title, homepage_url, crawling_method, s3_manager):
+    crawled_notice = crawling_method(homepage_url)
+    filtered_notice = s3_manager.duplication_filtering(crawled_notice)
+    if not filtered_notice:
+        return "empty"
+    else:
+        return f'{homepage_title}\n' + message_manager.make_message(filtered_notice)
